35337d7d1493091fb84fbfde159cbc9270dd2058
KimDeokwon <codevillain@naver.com>
2018-07-03 01:49:23 +0900

    Update Asset

---
+++ Bridge-iOS/Util/UI/UICustomTabBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  UICustomTabBarItem.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 7. 3..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class UICustomTabBarItem: UITabBarItem {
+
+}
