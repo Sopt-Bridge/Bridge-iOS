148c6206d21cf6b0ffdce9f42926a20f72b29683
KimDeokwon <codevillain@naver.com>
2018-07-02 22:25:10 +0900

    Add UIContentsCollectionViewCell

---
+++ Bridge-iOS/Util/UI/UIContentsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UIContentsCollectionViewCell.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 7. 2..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class UIContentsCollectionViewCell: UICollectionViewCell {
+    
+}
