5b2313469271fefc31366d17a746c39766d40f95
KimDeokwon <codevillain@naver.com>
2018-07-03 12:30:10 +0900

    Add SearchViewController

---
+++ Bridge-iOS/ViewController/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 7. 3..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
