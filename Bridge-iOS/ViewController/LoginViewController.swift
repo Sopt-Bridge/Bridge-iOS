02ca012b176a2f9224b38820dadb03dde13684d5
KimDeokwon <codevillain@naver.com>
2018-07-03 09:58:29 +0900

    Add facebook, google login

--- Bridge-iOS/ViewController/LoginViewController.swift
+++ Bridge-iOS/ViewController/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
     
@@ -16,22 +18,57 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func didClickedGoogleButton(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
     }
     
     @IBAction func didClickedFacebookButton(_ sender: UIButton) {
-    
+        
+        let manager: FBSDKLoginManager = FBSDKLoginManager.init()
+        manager.logIn(withReadPermissions: ["public_profile"], from: self) { (loginResult, error) in
+            if let result = loginResult {
+                let uid = result.token.userID // 이거 서버에 던져주기
+                print("uid: \(uid)")
+                let request: FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"picture.type(large), name"], tokenString: result.token.tokenString, version: nil, httpMethod: "GET")
+                
+                request.start(completionHandler: { (connection, result, error) in
+                    if(error == nil)
+                    {
+                        print("result \(result)")
+                    }
+                    else
+                    {
+                        print("error \(error)")
+                    }
+                })
+            }
+        }
     }
-    
     
     // MARK:- Override methdos
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance().signOut()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK:- GoogleSignInUIDelegate
+extension LoginViewController: GIDSignInUIDelegate {
+    func sign(inWillDispatch signIn: GIDSignIn!, error: Error!) {
+    }
+    
+    func sign(_ signIn: GIDSignIn!,
+              present viewController: UIViewController!) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    func sign(_ signIn: GIDSignIn!,
+              dismiss viewController: UIViewController!) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
