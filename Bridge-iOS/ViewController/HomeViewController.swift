275d5e1c44e0a9be71c817d606b9365be6961130
KimDeokwon <codevillain@naver.com>
2018-07-03 09:58:47 +0900

    Add banner auto scrolling funtion

--- Bridge-iOS/ViewController/HomeViewController.swift
+++ Bridge-iOS/ViewController/HomeViewController.swift
@@ -13,6 +13,7 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var bannerCollectionView: UICollectionView!
     @IBOutlet weak var contentsCollectionView: UICollectionView!
     var colorList: [UIColor] = [UIColor.black, UIColor.red, UIColor.green, UIColor.blue, UIColor.black, UIColor.red]
+    var timer: Timer!
     
     func presentToLoginView() {
         let viewIdentifier: String = "LoginViewController"
@@ -23,10 +24,20 @@ class HomeViewController: UIViewController {
         }
     }
     
+    func autoScrolling() {
+        var index: Int = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true, block: { timer in
+            index = (index + 1) % 4
+            let indexPath: IndexPath = IndexPath(row: index, section: 0)
+            self.bannerCollectionView.scrollToItem(at: indexPath, at: .left, animated: true)
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let indexPath: IndexPath = IndexPath(row: 1, section: 0)
         self.bannerCollectionView.scrollToItem(at: indexPath, at: .left, animated: false)
+        autoScrolling()
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,14 +48,16 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UIScrollViewDelegate {
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let fullyScrolledContentOffset:CGFloat = scrollView.frame.size.width * CGFloat(self.colorList.count - 1)
-        if (scrollView.contentOffset.x >= fullyScrolledContentOffset) {
-            let indexPath: IndexPath = IndexPath(row: 1, section: 0)
-            bannerCollectionView.scrollToItem(at: indexPath, at: .left, animated: false)
-        }
-        else if (scrollView.contentOffset.x == 0) {
-            let indexPath: IndexPath = IndexPath(row: self.colorList.count - 2, section: 0)
-            bannerCollectionView.scrollToItem(at: indexPath, at: .left, animated: false)
+        if scrollView.tag == 0 {
+            let fullyScrolledContentOffset:CGFloat = scrollView.frame.size.width * CGFloat(self.colorList.count - 1)
+            if (scrollView.contentOffset.x >= fullyScrolledContentOffset) {
+                let indexPath: IndexPath = IndexPath(row: 1, section: 0)
+                bannerCollectionView.scrollToItem(at: indexPath, at: .left, animated: false)
+            }
+            else if (scrollView.contentOffset.x == 0) {
+                let indexPath: IndexPath = IndexPath(row: self.colorList.count - 2, section: 0)
+                bannerCollectionView.scrollToItem(at: indexPath, at: .left, animated: false)
+            }
         }
     }
 }
@@ -56,15 +69,23 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == self.bannerCollectionView {
+        
+        switch collectionView.tag {
+        case 0:
             let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath)
             cell.backgroundColor = self.colorList[indexPath.item]
             return cell
-        }
-        else if collectionView == self.contentsCollectionView {
+        case 1:
             let cell: UIContentsCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentsCell", for: indexPath) as! UIContentsCollectionViewCell
             return cell
+        case 2:
+            let cell: UIContentsCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentsCell", for: indexPath) as! UIContentsCollectionViewCell
+            return cell
+        case 3:
+            let cell: UIContentsCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentsCell", for: indexPath) as! UIContentsCollectionViewCell
+            return cell
+        default:
+            return UICollectionViewCell()
         }
-        return UICollectionViewCell()
     }
 }
