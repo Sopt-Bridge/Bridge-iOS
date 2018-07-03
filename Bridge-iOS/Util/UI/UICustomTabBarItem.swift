a4917b7a2e5fe52a599d65ed99397c2d5d52d809
KimDeokwon <codevillain@naver.com>
2018-07-03 09:59:16 +0900

    Add CustomTabBarItem

--- Bridge-iOS/Util/UI/UICustomTabBarItem.swift
+++ Bridge-iOS/Util/UI/UICustomTabBarItem.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
+@IBDesignable
 class UICustomTabBarItem: UITabBarItem {
-
+    
+    @IBInspectable var top: CGFloat = 0 {
+        didSet {
+            let inset = self.imageInsets
+            self.imageInsets = UIEdgeInsets(top: self.top, left: inset.left, bottom: inset.bottom, right: inset.right)
+        }
+    }
+    
+    @IBInspectable var left: CGFloat = 0 {
+        didSet {
+            let inset = self.imageInsets
+            self.imageInsets = UIEdgeInsets(top: inset.top, left: self.left, bottom: inset.bottom, right: inset.right)
+        }
+    }
+    
+    @IBInspectable var bottom: CGFloat = 0 {
+        didSet {
+            let inset = self.imageInsets
+            self.imageInsets = UIEdgeInsets(top: inset.top, left: inset.left, bottom: self.bottom, right: inset.right)
+        }
+    }
+    
+    @IBInspectable var right: CGFloat = 0 {
+        didSet {
+            let inset = self.imageInsets
+            self.imageInsets = UIEdgeInsets(top: inset.top, left: inset.left, bottom: inset.bottom, right: self.right)
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        
+        let top = self.top
+        let left = self.left
+        let bottom = self.bottom
+        let right = self.right
+        
+        self.imageInsets = UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+        self.title = nil
+    }
 }
