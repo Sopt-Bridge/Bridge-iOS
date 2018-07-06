7ef2c0e60a30d4ba49f9758d51b797cb77612c09
KimDeokwon <codevillain@naver.com>
2018-07-06 11:26:01 +0900

    Add UIBannerCollectionViewCell
    Get repeat ui (banner collection view)

---
+++ Bridge-iOS/Util/UI/UIBannerCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  UIBannerCollectionViewCell.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 7. 4..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class BannerViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bannerCollectionView: UICollectionView!
+}
+
+class UIBannerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bannerImage: UIImageView!
+}
