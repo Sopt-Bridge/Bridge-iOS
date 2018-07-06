1be8388dcbe5742ecb69c83548181eeb990266a3
KimDeokwon <codevillain@naver.com>
2018-07-06 11:36:00 +0900

    Add InterpretationViewController

---
+++ Bridge-iOS/ViewController/InterpretationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InterpretationViewController.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 7. 5..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class InterpretationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let navigationController = self.navigationController {
+//            navigationController.navigationBar.layer.shadowOpacity = 0
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
