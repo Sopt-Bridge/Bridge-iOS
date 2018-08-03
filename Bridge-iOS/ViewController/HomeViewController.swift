f55fb53f9958a7a30db59738bd933e6873a1dc04
KimDeokwon <codevillain@naver.com>
2018-08-03 18:40:49 +0900

    Add HomeViewController

---
+++ Bridge-iOS/ViewController/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 8. 3..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self.navigationController?.navigationBar.frame.height)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
