eed49f261f1a770d389f9f05f02267903177f790
KimDeokwon <codevillain@naver.com>
2018-08-03 18:40:57 +0900

    Add RadioButtonGroup

---
+++ Bridge-iOS/Util/UI/RadioButtonGroup.swift
@@ -0,0 +1,17 @@
+//
+//  RadioButtonGroup.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 8. 3..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class RadioButtonGroup: UIView {
+
+    @IBInspectable var selectedColor: UIColor = UIColor.clear
+    @IBInspectable var diselectedColor: UIColor = UIColor.clear
+
+    
+}
