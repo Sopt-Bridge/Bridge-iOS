c1f4a645cf950217ea73d97e76beb914e3eb1d0a
KimDeokwon <codevillain@naver.com>
2018-08-07 14:31:36 +0900

    Add RadioButton

---
+++ Bridge-iOS/Util/UI/RadioButton.swift
@@ -0,0 +1,21 @@
+//
+//  RadioButton.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 8. 7..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class RadioButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
