4ba81d0ca3e4aa1669cc767df7a0c72cadd66c99
KimDeokwon <codevillain@naver.com>
2018-08-18 23:24:17 +0900

    Add NSObjectExtension

---
+++ Bridge-iOS/Util/Extension/NSObjectExtension.swift
@@ -0,0 +1,16 @@
+//
+//  NSObjectExtension.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 8. 17..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+
+    static var distinctIdentifier: String {
+        return String(describing: self)
+    }
+}
