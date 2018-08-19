74462b964a7e4ba0155982a0a59f296a9274c3ce
KimDeokwon <codevillain@naver.com>
2018-08-19 23:23:28 +0900

    Add UIBannerCollectionViewCell

---
+++ Bridge-iOS/Util/UI/CollectionView/Cell/UIBannerCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UIBannerCollectionViewCell.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 8. 19..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class UIBannerCollectionViewCell: UIHomeViewCollectionViewCell {
+
+    @IBOutlet weak var bannerCollectionView: UICollectionView!
+}
