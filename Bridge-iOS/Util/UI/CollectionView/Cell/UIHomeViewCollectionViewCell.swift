55d0ba4d83f36be7be47cf85ee2c0da6f9513a71
KimDeokwon <codevillain@naver.com>
2018-08-19 23:23:19 +0900

    Add UIHomeViewCollectionViewCell

---
+++ Bridge-iOS/Util/UI/CollectionView/Cell/UIHomeViewCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewCollectionViewCell.swift
+//  Bridge-iOS
+//
+//  Created by 김덕원 on 2018. 8. 19..
+//  Copyright © 2018년 codevillain. All rights reserved.
+//
+
+import UIKit
+
+class UIHomeViewCollectionViewCell: UICollectionViewCell {
+
+    let cellIdentifier: String = "ContentCell"
+}
